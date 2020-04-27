d915ae5412d7430368cbd67e5aa964acf9544238
Slattz <Slattz@users.noreply.github.com>
2020-04-27 16:50:04 +0100

    use Dermots sample driver

---
+++ PlayListSampleDriver.java
@@ -0,0 +1,31 @@
+import java.util.Collections;
+public class PlayListSampleDriver {
+    public static void main(String[] args) {
+        PlayList myFavourites = new PlayList();
+        myFavourites.add("Like a Rolling Stone","Bob Dylan");
+        myFavourites.add("(I Can't Get No) Satisfaction","The Rolling Stones",1965,197);
+        myFavourites.add("Imagine", "John Lennon", 1971, 215);
+        myFavourites.add("What's Going On", "Marvin Gaye");
+        myFavourites.add("Respect", "Aretha Franklin");
+        myFavourites.add("Good Vibrations", "Some Artist");
+        myFavourites.add("Hey Jude", "The Beatles", 1968, 347);
+        myFavourites.add("Smells Like Teen Spirit", "Nirvana");
+        myFavourites.add("What'd I Say", "Ray Charles");
+        myFavourites.add("The Long and Winding Road", "The Beatles");
+        myFavourites.add("The Times They are a Changin'","Bob Dylan");
+        myFavourites.add("Yellow Submarine", "The Beatles", 1967, 235);
+        System.out.println();
+        myFavourites.showList();
+        System.out.println();
+        myFavourites.playAll(false);
+        System.out.println();
+        System.out.println();
+        myFavourites.playAll(true);
+        System.out.println();
+        myFavourites.playOnly("Beatles");
+        System.out.println();
+        myFavourites.playOnly(1971);
+        myFavourites.remove("WHAT'D I SAY");
+        myFavourites.showList();
+    }
+}
